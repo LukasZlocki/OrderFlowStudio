84de24611ad2f704b4131fccd10f7f0652106454
lzlocki <lzlocki@poczta.fm>
2021-04-07 21:25:18 +0200

    Add changes to cover db init and update operations

--- OrderFlowStudio.Api/Startup.cs
+++ OrderFlowStudio.Api/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OrderFlowStudio.Data;
 
 namespace OrderFlowStudio.Api
 {
@@ -24,6 +26,12 @@ namespace OrderFlowStudio.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddControllers();
+
+            services.AddDbContext<OrderDbContext>(Options => 
+                Options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ OrderFlowStudio.Data/OrderDbContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using OrderFlowStudio.Data.Models;
+
+namespace OrderFlowStudio.Data
+{
+    public class OrderDbContext : DbContext
+    {
+        public OrderDbContext(DbContextOptions<OrderDbContext> options)
+        : base(options)
+        {
+            
+        }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderRaport> OrderRaports { get; set; }
+        public DbSet<Product> Products { get; set; }
+        
+    }
+}
