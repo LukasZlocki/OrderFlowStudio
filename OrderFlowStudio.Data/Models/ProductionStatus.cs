456ee173776702960e84dd4b6447d590a0099c67
lzlocki <lzlocki@poczta.fm>
2023-02-03 13:12:18 +0100

    Add production status model

---
+++ OrderFlowStudio.Data/Models/ProductionStatus.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace OrderFlowStudio.Data.Models
+{
+    public class ProductionStatus
+    {
+        public int StatusId { get; set; }
+        public int StatusCode { get; set; }
+    }
+}
