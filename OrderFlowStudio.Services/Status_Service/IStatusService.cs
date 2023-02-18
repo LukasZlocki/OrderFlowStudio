c81c6e14eb06b8539a6f7d2e34839de33f4977b5
lzlocki <lzlocki@poczta.fm>
2023-02-18 17:51:53 +0100

    Add status service

---
+++ OrderFlowStudio.Services/Status_Service/IStatusService.cs
@@ -0,0 +1,8 @@
+namespace OrderFlowStudio.Services.Status_Service
+{
+    public interface IStatusService
+    {
+         // READ
+         public int GetStatusIdByStatusNumber(int statusNb);
+    }
+}
