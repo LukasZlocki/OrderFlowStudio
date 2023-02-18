63d885cb384f04bccc8240ff5311f6b821026a12
lzlocki <lzlocki@poczta.fm>
2023-02-18 17:39:31 +0100

    Add raportoncreate object prepared for creating order contoller method

---
+++ OrderFlowStudio.Api/Dtos/OrderRaportOnCreateDto.cs
@@ -0,0 +1,9 @@
+namespace OrderFlowStudio.Api.Dtos
+{
+    public class OrderRaportOnCreateDto
+    {
+        public int QuantityFinished { get; set; }
+        public int OrderId { get; set; }
+        public int StatusId { get; set; }
+    }
+}
