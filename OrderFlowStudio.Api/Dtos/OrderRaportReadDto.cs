36641bbfa3e7bf19e39bb819465a73d60354aea1
lzlocki <lzlocki@poczta.fm>
2023-02-19 14:31:42 +0100

    Remove orderid from Dto read model

--- OrderFlowStudio.Api/Dtos/OrderRaportReadDto.cs
+++ OrderFlowStudio.Api/Dtos/OrderRaportReadDto.cs
@@ -5,6 +5,5 @@ namespace OrderFlowStudio.Api.Dtos
         public int Id { get; set; }
         public int QuantityFinished { get; set; }
         public ProductionStatusReadDto StatusDto { get; set; }     
-        public OrderReadDto OrderDto { get; set; }
     }
 }
