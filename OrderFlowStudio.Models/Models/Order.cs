bbfdb5a703631878ec88b26c320023672f6316a6
lzlocki <lzlocki@poczta.fm>
2024-02-24 12:53:22 +0100

    Add models

---
+++ OrderFlowStudio.Models/Models/Order.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace OrderFlowStudio.Models.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public int OrderNumber { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+
+        public int RaportId { get; set; }
+        [ForeignKey("RaportId")]
+        public OrderReport Raport { get; set; }
+    }
+}
