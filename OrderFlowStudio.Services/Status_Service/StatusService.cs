cd51991098cfd1d899f3afcd5949f250bfd46d81
lzlocki <lzlocki@poczta.fm>
2024-02-25 09:13:55 +0100

    Add status services

---
+++ OrderFlowStudio.Services/Status_Service/StatusService.cs
@@ -0,0 +1,22 @@
+using OrderFlowStudio.Models;
+
+namespace OrderFlowStudio.Services.Status_Service
+{
+    public class StatusService : IStatusService
+    {
+        private readonly OrderDbContext _db;
+
+        public StatusService(OrderDbContext db)
+        {
+            _db = db;
+        }
+
+        // READ
+        public int GetStatusIdByStatusNumber(int statusNb)
+        {
+            var status = _db.Statuses.Where(st => st.StatusCode == statusNb).FirstOrDefault();
+            int statusId = status.StatusId;
+            return statusId;
+        }
+    }
+}
