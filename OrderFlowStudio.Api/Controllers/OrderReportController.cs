4cfc640a28cd66d650854ed9e243ee3f5abac42d
lzlocki <lzlocki@poczta.fm>
2024-02-25 12:45:47 +0100

    Add controllers

---
+++ OrderFlowStudio.Api/Controllers/OrderReportController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderFlowStudio.Services.OrderReport_Service;
+
+namespace OrderFlowStudio.Api.Controllers
+{
+    [ApiController]
+    public class OrderRaportController : ControllerBase
+    {
+        private readonly IOrderReportService _orderRaportService;
+
+        public OrderRaportController(IOrderReportService orderRaportService)
+        {
+            _orderRaportService = orderRaportService;
+        }
+
+        // READ
+        [HttpGet("api/raport/{id}")]
+        public ActionResult GetRaport(int id)
+        {
+            var raport = _orderRaportService.GetOrderReportById(id);
+            var raportReadDto = OrderReportMapper.SerializeOrderReportToOrderReportReadDto(raport);
+            return Ok(raportReadDto);
+        }
+
+        // READ
+        [HttpGet("api/raport")]
+        public ActionResult GetRaports()
+        {
+            var raports = _orderRaportService.GetAllOrderReports();
+            var raportsReadDto = OrderReportMapper.SerializeListOfOrderReportsToOrderReportReadDtoList(raports);
+            return Ok(raportsReadDto);
+        }
+
+        // UPDATE
+        [HttpPatch("api/raport")]
+        public ActionResult UpdateRaportStatuses([FromBody] OrderReportReadDto raportReadDto)
+        {
+            var raport = OrderReportMapper.SerializeStatusesOnlyOfOrderReportReadDtoToOrderReport(raportReadDto);
+            var serviceResponse = _orderRaportService.UpdateOrderReportStatuses(raport);
+            return Ok(serviceResponse);
+
+        }
+
+        // DELETE
+        // No need to code the functionality !
+
+    }
+}
