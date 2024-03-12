6eb0b6606c8885004d3fc66494519822320ada61
lzlocki <lzlocki@poczta.fm>
2024-03-12 21:45:37 +0100

    Add  methods to retrive status model.

--- OrderFlowStudio.Services/Status_Service/StatusService.cs
+++ OrderFlowStudio.Services/Status_Service/StatusService.cs
@@ -1,4 +1,5 @@
 using OrderFlowStudio.Models;
+using OrderFlowStudio.Models.Models;
 
 namespace OrderFlowStudio.Services.Status_Service
 {
@@ -17,6 +18,18 @@ namespace OrderFlowStudio.Services.Status_Service
             var status = _db.Statuses.Where(st => st.StatusCode == statusNb).FirstOrDefault();
             int statusId = status.StatusId;
             return statusId;
+        }
+
+        public ProductionStatus GetStatusModelByStatusId(int statusId)
+        {
+            var status = _db.Statuses.Where(id => id.StatusId == statusId).FirstOrDefault();
+            return status;
+        }
+
+        public ProductionStatus GetStatusModelByStatusNumber(int statusNumber)
+        {
+            var status = _db.Statuses.Where(nb => nb.StatusCode == statusNumber).FirstOrDefault();
+            return status;
         }
     }
 }
